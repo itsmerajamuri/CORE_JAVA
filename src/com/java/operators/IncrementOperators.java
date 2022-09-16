8535a08d1276c79efbefad74214c54497704ee73
itsmerajamuri <rajamuripavan558@gmail.com>
2022-09-16 17:15:58 +0530

    day 2

---
+++ src/com/java/operators/IncrementOperators.java
@@ -0,0 +1,31 @@
+package com.java.operators;
+
+public class IncrementOperators {
+
+	public static void main(String[] args) {
+		// ++(pre-increment)
+		int a=10;
+		int b=++a;
+		System.out.println("a value is " +b +" && "+"b value is "+a);
+		// ++(post-increment)
+		int c=20;
+		int d=c++;
+		System.out.println("c value is " +c +" && "+"d value is "+d);
+		// ++(pre-decrement)
+				int e=10;
+				int f=--e;
+				System.out.println("e value is " +e +" && "+"f value is "+f);
+				// ++(post-decrement)
+				int g=20;
+				int h=g--;
+				System.out.println("g value is " +g +" && "+"h value is "+h);
+
+				
+
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/java/operators/Operators.java
@@ -0,0 +1,36 @@
+package com.java.operators;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// +
+		int a,b;
+		a=10;
+		b=20;
+		a=10;
+		b=20;
+		System.out.println(a+b);
+		// -
+		int c,d;
+		c=20;
+		d=1;
+		System.out.println(c-d);
+		// *
+		int e,f;
+		e=20;
+		f=5;
+		System.out.println(e*f);
+		// /
+		int g,h;
+		g=20;
+		h=5;
+		System.out.println(g/h);
+		// %
+		int i,j;
+		i=20;
+		j=2;
+		System.out.println(i%j);
+
+	}
+
+}
